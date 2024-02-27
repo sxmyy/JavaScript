7575920565faf74af90d643da454cda8fdcea395
joão victor <125697762+sxmyy@users.noreply.github.com>
2024-02-27 16:58:11 -0300

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-const button = document.getElementById("button");
-const text = document.getElementById("h1");
-let randomNumber;
-
-button.onclick = function(){
-    randomNumber = Math.floor(Math.random() * 100);
-    text.textContent = randomNumber;
-}
